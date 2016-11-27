dd71c4425eaac7b1f49ff0c79c3270c17187bfae
kurokouda <zjam.ubias@gmail.com>
2016-11-27 21:35:52 +0800

    Change main method to an object

--- src/__main__.py
+++ src/__main__.py
@@ -1,31 +1,33 @@
 import pygame
 
 from .config import Config
-from .context import Context
+# from .context import Context
 
 
-def _main():
-    pygame.init()
-    Context().clock = pygame.time.Clock()
-    print(Config().__dict__)
+class Main(object):
+    def __init__(self):
+        pygame.init()
+        self._clock = pygame.time.Clock()
+        self._window_width = Config().window.WIDTH
+        self._window_height = Config().window.HEIGHT
+        self._screen = pygame.display.set_mode(
+                (Config().window.WIDTH, Config().window.HEIGHT))
+        self._running = False
 
-    screen = pygame.display.set_mode((Config().window.WIDTH,
-            Config().window.HEIGHT))
-    running = True
+    def run(self):
+        self._running = True
+        while self._running:
+            self._clock.tick(Config().window.FPS)
+            pygame.display.set_caption('FPS: {:02.2f}, Time: {:.2f}'.format(
+                        self._clock.get_fps(), pygame.time.get_ticks() * 0.001
+                        ))
 
-    while running:
-        Context().clock.tick(Config().window.FPS)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self._running = False
 
-        pygame.display.set_caption('FPS: {:02.2f}, Time: {:.2f}'.format(
-                    Context().clock.get_fps(), pygame.time.get_ticks() * 0.001
-        ))
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-        screen.fill(pygame.Color('gray20'))
-        pygame.display.flip()
+            self._screen.fill(pygame.Color('gray20'))
+            pygame.display.flip()
 
 if __name__ == '__main__':
-    _main()
+    Main().run()
