c1b1adbf0708e579f2353abd977d6c1053e5bf40
kurokouda <zjam.ubias@gmail.com>
2016-11-28 02:25:32 +0800

    Comment out ununsed code

--- src/gameworld.py
+++ src/gameworld.py
@@ -1,45 +1,44 @@
 from copy import copy
-from random import random
-from math import pi as PI
+# from random import random
+# from math import pi as PI
 
 from .config import Config
 from .d2.vector2d import Vector2D
 from .cellspacepartition import CellSpacePartition
-from .path import Path
+# from .path import Path
 from .entity.entityfunctiontemplates import (
     enforce_non_penetration_constraint,
     tag_neighbors,
 )
-from .utils import random_clamped
-from .entity.vehicle import Vehicle
+# from .utils import random_clamped
+# from .entity.vehicle import Vehicle
 from .smoother import Smoother
 
 
 class GameWorld(object):
-    __slots__ = (
-        '_vehicles',
-        '_obstacles',
-        '_walls',
-        '_cell_space',
-        '_path',
-        '_paused',
-        '_window_height',
-        '_window_width',
-        '_crosshair',
-        '_average_frame_time',
-        '_show_walls',
-        '_show_obstacles',
-        '_show_path',
-        '_show_detection_box',
-        '_show_wander_circle',
-        '_show_feelers',
-        '_show_steering_force',
-        '_show_fps',
-        '_render_neighbors',
-        '_view_keys',
-        '_show_cell_space_info',
-    )
-
+    # __slots__ = (
+    #     '_vehicles',
+    #     '_obstacles',
+    #     '_walls',
+    #     '_cell_space',
+    #     '_path',
+    #     '_paused',
+    #     '_window_height',
+    #     '_window_width',
+    #     '_crosshair',
+    #     '_average_frame_time',
+    #     '_show_walls',
+    #     '_show_obstacles',
+    #     '_show_path',
+    #     '_show_detection_box',
+    #     '_show_wander_circle',
+    #     '_show_feelers',
+    #     '_show_steering_force',
+    #     '_show_fps',
+    #     '_render_neighbors',
+    #     '_view_keys',
+    #     '_show_cell_space_info',
+    # )
     SAMPLE_RATE = 10
     _frame_rate_smoother = Smoother(SAMPLE_RATE, 0.0)
 
@@ -65,52 +64,55 @@ class GameWorld(object):
         self._view_keys = False
         self._show_cell_space_info = False
 
+        self._set_cell_space()
+
         # NOTE: Code below won't apply for project implementation. Encapsulate
         # in a function for overriding.
+
+        # border = 30
+        # self._path = Path(5, border, border, window_width - border,
+        #         window_height - border, True)
+        #
+        # for _ in range(Config().NUM_AGENTS):
+        #     x = window_width // 2 + random_clamped() * window_width // 2
+        #     y = window_height // 2 + random_clamped() * window_height // 2
+        #     spawn_pos = Vector2D(x, y)
+        #
+        #     vehicle = Vehicle(
+        #             world=self,
+        #             position=spawn_pos,
+        #             rotation=random() * 2 * PI,
+        #             velocity=Vector2D(),
+        #             mass=Config().VEHICLE_MASS,
+        #             max_force=Config().STEERING_FORCE,
+        #             max_speed=Config().MAX_SPEED,
+        #             max_turn_rate=Config().MAX_TURN_RATE_PER_SECOND,
+        #             scale=Config().VEHICLE_SCALE
+        #             )
+        #
+        #     vehicle.steering.flocking_on()
+        #     self._vehicles.append(vehicle)
+        #     self._cell_space.add_entity(vehicle)
+        #
+        # SHOAL = False
+        # if SHOAL:
+        #     LAST = Config().NUM_AGENTS - 1
+        #     self._vehicles[LAST].steering.flocking_off()
+        #     self._vehicles[LAST].scale = Vector2D(10, 10)
+        #     self._vehicles[LAST].steering.wander_on()
+        #     self._vehicles[LAST].max_speed = 70
+        #
+        #     for i in range(LAST):
+        #         self._vehicles[i].steering.evade_on(self._vehicles[LAST])
+
+    def _set_cell_space(self):
         self._cell_space = CellSpacePartition(
-                width=window_width,
-                height=window_height,
+                width=self._window_width,
+                height=self._window_height,
                 cells_x=Config().NUM_CELLS_X,
                 cells_y=Config().NUM_CELLS_Y,
                 max_entities=Config().NUM_AGENTS
                 )
-
-        border = 30
-        self._path = Path(5, border, border, window_width - border,
-                window_height - border, True)
-
-        for _ in range(Config().NUM_AGENTS):
-            x = window_width // 2 + random_clamped() * window_width // 2
-            y = window_height // 2 + random_clamped() * window_height // 2
-            spawn_pos = Vector2D(x, y)
-
-            vehicle = Vehicle(
-                    world=self,
-                    position=spawn_pos,
-                    rotation=random() * 2 * PI,
-                    velocity=Vector2D(),
-                    mass=Config().VEHICLE_MASS,
-                    max_force=Config().STEERING_FORCE,
-                    max_speed=Config().MAX_SPEED,
-                    max_turn_rate=Config().MAX_TURN_RATE_PER_SECOND,
-                    scale=Config().VEHICLE_SCALE
-                    )
-
-            vehicle.steering.flocking_on()
-            self._vehicles.append(vehicle)
-            self._cell_space.add_entity(vehicle)
-
-        SHOAL = False
-        if SHOAL:
-            LAST = Config().NUM_AGENTS - 1
-            self._vehicles[LAST].steering.flocking_off()
-            self._vehicles[LAST].scale = Vector2D(10, 10)
-            self._vehicles[LAST].steering.wander_on()
-            self._vehicles[LAST].max_speed = 70
-
-            for i in range(LAST):
-                self._vehicles[i].steering.evade_on(self._vehicles[LAST])
-
 
     def non_penetration_constraint(self, vehicle):
         enforce_non_penetration_constraint(vehicle, self._vehicles)
