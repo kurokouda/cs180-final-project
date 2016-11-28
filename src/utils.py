bfe79ba4ccc3c7d870e5b29d6799c0058d871e53
kurokouda <zjam.ubias@gmail.com>
2016-11-28 11:19:34 +0800

    Add function to get rand point from triangle

--- src/utils.py
+++ src/utils.py
@@ -7,3 +7,9 @@ def random_in_range(low, high):
 def random_clamped():
     '''Return a random double from -1 to 1'''
     return random() - random()
+
+def random_point_in_tri(pt_a, pt_b, pt_c):
+    r1 = random()
+    r2 = random()
+    return (((1 - r1**0.5) * pt_a) + ((r1**0.5 * (1 - r2)) * pt_b) +
+            ((r1**0.5 * r2) * pt_c))
