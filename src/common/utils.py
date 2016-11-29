fe2463e8827cd4c7da647ce7eac1e677326ff2e2
kurokouda <zjam.ubias@gmail.com>
2016-11-30 04:42:18 +0800

    Create initial utils.py with cantor pair function

---
+++ src/common/utils.py
@@ -0,0 +1,8 @@
+
+def __cantor_biject(k):
+    return k * 2 if k >= 0 else -k * 2 - 1
+
+def cantor(a, b):
+    k1 = __cantor_biject(a)
+    k2 = __cantor_biject(b)
+    return round(0.5 * (k1 + k2) * (k1 + k2 + 1) + k2)
