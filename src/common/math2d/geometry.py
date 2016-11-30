33cfefef85246081314ac14c9fe932d2d5005135
kurokouda <zjam.ubias@gmail.com>
2016-12-01 02:02:27 +0800

    Modify geometry to use index rather than attrs.

--- src/common/math2d/geometry.py
+++ src/common/math2d/geometry.py
@@ -1,7 +1,7 @@
 
 def two_circles_overlapped(center_a, radius_a, center_b, radius_b):
-    distance_between_centers = ((center_a.x - center_b.x)**2 +
-            (center_a.y - center_b.y)**2)**0.5
+    distance_between_centers = ((center_a[0] - center_b[0])**2 +
+            (center_a[1] - center_b[1])**2)**0.5
 
     return (distance_between_centers < radius_a + radius_b or
             distance_between_centers < abs(radius_a - radius_b))
