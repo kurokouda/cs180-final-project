5d7122bb1b3c13aadb04c34b8137cb9a15391db5
kurokouda <zjam.ubias@gmail.com>
2016-12-01 02:03:39 +0800

    Add rand_in_range()

--- src/common/utils.py
+++ src/common/utils.py
@@ -1,3 +1,5 @@
+from random import random
+
 
 def __cantor_biject(k):
     return k * 2 if k >= 0 else -k * 2 - 1
@@ -6,3 +8,6 @@ def cantor(a, b):
     k1 = __cantor_biject(a)
     k2 = __cantor_biject(b)
     return round(0.5 * (k1 + k2) * (k1 + k2 + 1) + k2)
+
+def random_in_range(a, b):
+    return a + random() * (b - a)
